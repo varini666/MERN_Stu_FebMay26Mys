b279526f3c082741085ed38030edbd12c1229e04
varini666 <varinivarini925@gmail.com>
2026-03-03 15:25:45 +0530

    Logical Operators

---
+++ W3/D2/P3.js
@@ -0,0 +1,6 @@
+//Logical Operators
+console.log("Logical Operators");
+let a = true ,b = false;
+console.log("a&&b=:",a&&b);//Logical AND
+console.log("a||b=:",a||b);//Logical OR
+console.log("!a=:",!a);//Logical NOT
