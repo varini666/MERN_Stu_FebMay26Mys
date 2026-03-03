9a302a91a8488fac343dfee2240ee21d2a465523
varini666 <varinivarini925@gmail.com>
2026-03-03 15:25:02 +0530

    Comparison Operators

---
+++ W3/D2/P2.js
@@ -0,0 +1,11 @@
+//Comparison Operators
+console.log("Comparison Operators");
+let x = 2, y = '3';
+console.log("x==y:",x==y);//Loose equality(values are cmp not DT)
+console.log("x===y:",x===y);//strict equality (values & DT is cmp)
+console.log("x!=y:",x!=y);//Loose inequality
+console.log("x!==y:",x!==y);//strict equality
+console.log("x>3:",x>3);
+console.log("x<3:",x<3);
+console.log("x>=3:",x>=3);
+console.log("x<=3:",x<=3);
