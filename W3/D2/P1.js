775c83fa6fa0d0835ffe1bad27edb3e81ecc4585
varini666 <varinivarini925@gmail.com>
2026-03-03 15:24:20 +0530

    Arithmetic Operators

---
+++ W3/D2/P1.js
@@ -0,0 +1,14 @@
+//Arithmetic Operators
+console.log("Arithmetic Operators");
+const a = 2, b = 3;
+let=0
+console.log("a+b:",c=a+b);
+console.log("a-b:",c=a-b);
+console.log("a*b:",c=a*b);
+console.log("a/b:",c=a/b);
+console.log("a%b:",c=a%b);//modulus
+console.log("a**b:",c=a**b);//exponential
+// console.log("a++:",a++);//post-increment
+// console.log("++a:",++a);//pre-increment
+// console.log("a--:",a--);//post-decrement
+// console.log("--a:",--a);//pre-decrement
