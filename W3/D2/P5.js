a346ab28691ef5359c6f75803804328752ee3aea
varini666 <varinivarini925@gmail.com>
2026-03-03 15:42:49 +0530

    Function Declaration

---
+++ W3/D2/P5.js
@@ -0,0 +1,9 @@
+// Function Declaration
+// Function greet(name);
+greet("varini");
+// Function Declaration/Definition
+function greet(name) {
+    console.log("Hello, "+name+" !");
+}
+//Function call
+greet("varini");
