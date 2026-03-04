c5f3c797e1cf723cfb42e4ca61bcbb5080ef3d58
varini666 <varinivarini925@gmail.com>
2026-03-04 13:45:03 +0530

    Basics of Objects

--- W3/D3/P5.js
+++ W3/D3/P5.js
@@ -8,3 +8,20 @@ const person =
 console.log("Person",person);
 console.log("Name:",person.name);//Dot notation
 console.log("Age:",person['age']);//Bracket notation
+
+// Add a new property
+person.city = "Mysore";
+console.log("Person",person);
+// /Modify
+person.age=22;
+console.log("Person",person);
+// delete
+delete person.isStudent;
+console.log("Person",person);
+
+// Object constructor
+const car = new Object();
+car.make="Audi";
+car.model="A4";
+car.year=2026;
+console.log("car",car);
