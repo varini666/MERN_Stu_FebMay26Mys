631294fda525f31ec3f6fab8c928bf428e2d0710
varini666 <varinivarini925@gmail.com>
2026-03-04 12:38:38 +0530

    while loop

---
+++ W3/D3/P2.js
@@ -0,0 +1,14 @@
+//while loop
+console.log("while loop examples");
+
+let count=0;
+while(count<5){
+    console.log("count:",count);
+    count++;
+}
+//do-while loop
+let value=0;
+do{
+    console.log("value:",value);
+    value++;
+}while(value<3);
