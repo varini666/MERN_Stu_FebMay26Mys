bc83dbb44249e825aefecedff7c8d9359d5503f1
varini666 <varinivarini925@gmail.com>
2026-03-04 12:39:14 +0530

    Break & continue

---
+++ W3/D3/P3.js
@@ -0,0 +1,15 @@
+//Break
+for(let i=0;i<5;i++){
+    if(i==2){
+    break;
+    }
+    console.log("i =",i);// forward loop
+}
+
+// continue
+for(let i=0;i<5;i++){
+    if(i==2){
+    continue;
+    }
+    console.log("i =",i);
+}
