0eb152d0a46ead85618550f9e90481fe201c7844
varini666 <varinivarini925@gmail.com>
2026-03-04 12:40:14 +0530

    Basics of Objects

---
+++ W3/D3/P5.js
@@ -0,0 +1,10 @@
+//Basics of Objects
+const person =
+{
+    name:'varini',
+    age: 21,
+    isStudent: false
+};
+console.log("Person",person);
+console.log("Name:",person.name);//Dot notation
+console.log("Age:",person['age']);//Bracket notation
