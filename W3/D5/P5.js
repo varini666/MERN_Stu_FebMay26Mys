58c8611f1cb07ad15c4a56e9b1462a72fe78566a
varini666 <varinivarini925@gmail.com>
2026-03-06 16:39:59 +0530

    Named Handlers and removeEventListeners.js

---
+++ W3/D5/P5.js
@@ -0,0 +1,11 @@
+const startBtn = document.getElementById("startBtn");
+const removeBtn = document.getElementById("removeBtn");
+
+function handleClick(){
+    console.log("startBtn clicked");
+}
+startBtn.addEventListener("click",handleClick);
+removeBtn.addEventListener("click",function(){
+    startBtn.removeEventListener("click",handleClick);
+    console.log("click listner removed from startBtn");
+});
