a0dc4165d15936a3a6402a0f9f43773eeb976c4d
varini666 <varinivarini925@gmail.com>
2026-03-07 22:21:30 +0530

    wallet

---
+++ W3/D3/A6.js
@@ -0,0 +1,9 @@
+//wallet
+const wallet=new Object();
+wallet.owner="varini";
+wallet.balance="10000";
+wallet.lastTransaction="null";
+console.log(wallet);
+function wallet(deposite,withdraw){
+    
+}
