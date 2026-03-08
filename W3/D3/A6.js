9a331ce3e0b852c57fe69989ba192c1fddc6c81d
varini666 <varinivarini925@gmail.com>
2026-03-08 19:45:11 +0530

    wallet

--- W3/D3/A6.js
+++ W3/D3/A6.js
@@ -4,6 +4,17 @@ wallet.owner="varini";
 wallet.balance="10000";
 wallet.lastTransaction="null";
 console.log(wallet);
-function wallet(deposite,withdraw){
-    
+function deposite(amount){
+    this.balance = amount;
+    console.log("amount");
+    console.log("this.balance");
+}
+function withdraw(amount){
+    if (amount<=balance){
+        this.balance=amount;
+    console.log("amount");
+    console.log("this.balance");
+    }else {
+        console.log("insufficient balance");
+    }
 }
