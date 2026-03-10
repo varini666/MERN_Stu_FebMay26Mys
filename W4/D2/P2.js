a60430f1fc13300c020391d4e6c5f62a1699fc8d
varini666 <varinivarini925@gmail.com>
2026-03-10 10:38:17 +0530

    CreateElement() & appendChild()

---
+++ W4/D2/P2.js
@@ -0,0 +1,23 @@
+// CreateElement() & appendChild()
+
+const list = document.getElementById("list");
+let counter = 1;
+
+document.getElementById("addBtn").addEventListener("click",
+    function(){
+        errmsg.textContent = " ";
+        const li = document.createElement("li");
+        li.textContent = "Item" + counter++;
+        list.appendChild(li);
+    }
+);
+
+document.getElementById("rmBtn").addEventListener("click",
+    function(){
+        if(list.lastElementChild) {
+            list.removeChild(list.lastElementChild)
+        } 
+        else {
+            errmsg.textContent = "No items to dlt";
+        }
+    });
