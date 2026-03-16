c89bfa1edb31174087d7515facf25586080fbabd
varini666 <varinivarini925@gmail.com>
2026-03-16 22:59:04 +0530

    projectsData.js

--- W4/D5/portfolio/js/data/projects.js
+++ W4/D5/portfolio/js/data/projects.js
@@ -4,7 +4,7 @@ const projectsData = [
     name: "Blue Collar",
     category: "MERN",
     description:
-      "Blue-collar jobs are occupations that involve manual work or skilled trades, commonly found in construction manufacturing, transportation, and maintenance. These jobs include electricians, plumbers, factory workers, mechanics, and truck drivers.",
+      "Blue-collar jobs are occupations that involve manual work or skilled trades, commonly found in construction manufacturing, transportation, and maintenance.",
     technologies: ["ReactJS", "ExpressJS", "MongoDB"],
     status: "Live",
     liveDemo: "#",
@@ -15,7 +15,7 @@ const projectsData = [
     name: "Agri Driven Agri Tech App",
     category: "",
     description:
-      "This Streamlet application is designed to offer a comprehensive suite of agricultural services through an intuitive, user-friendly interface. It includes features such as weather forecasting, crop recommendation,disease prediction, yield estimation, and insurance risk assessment, all accessible via a navigable sidebar menu..",
+      "This Streamlet application is designed to offer a comprehensive suite of agricultural services through an intuitive, user-friendly interface.",
     technologies: ["python", "HTML", "CSS","JS"],
     status: "Live",
     liveDemo: "#",
