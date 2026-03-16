fc7ec2edba07b3466dc6ea3ed6d5344b2b424484
varini666 <varinivarini925@gmail.com>
2026-03-16 16:33:49 +0530

    package.json

---
+++ W5/D1/P5.sh
@@ -0,0 +1,5 @@
+set -e
+mkdir -p node-learning-project #p It is a flag which prevents error if the parent folder is already exist
+cd node-learning-project #call directory
+npm init -y #y initialize yes
+echo "Project Created in $(pwd)"
