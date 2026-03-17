22eaf661fb68a697071f67cc302a195e3cfeaa6f
varini666 <varinivarini925@gmail.com>
2026-03-17 15:37:50 +0530

    chaining promises with return values.js

---
+++ W5/D2/P10.js
@@ -0,0 +1,20 @@
+// chaining promises with return values
+function getBaseAmount(){
+    return new Promise(function(resolve){
+        setTimeout(function(){
+            resolve(1000);//1000 is the price, base amt
+        },500);
+    });
+}
+getBaseAmount()
+.then(function(amount){
+    console.log("Base amount:",amount);
+    return amount+200;
+})
+.then(function(updatedAmount){
+    console.log("Amount after service charge:",updatedAmount);
+    return updatedAmount - 100;
+})
+.then(function(finalAmount){
+    console.log("Final Amount after discount:",finalAmount);
+});
