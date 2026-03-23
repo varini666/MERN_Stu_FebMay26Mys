1f993a87a4c7d7952e821a2e42a5720d795e14ac
varini666 <varinivarini925@gmail.com>
2026-03-23 12:35:33 +0530

    commonJS export and import.js

---
+++ W6/D1/P2.js
@@ -0,0 +1,15 @@
+//commonJS export and import
+
+function calculateTax(amount){
+    return amount*0.18;
+}
+
+function applyDiscount(amount,percent){
+    return amount-amount*percent/100;
+}
+
+function formatCurrency(amount){
+    return "INR"+amount.toFixed(2);
+}
+//module.exports makes this function avail to require()
+module.exports = {calculateTax,applyDiscount,formatCurrency}; 
