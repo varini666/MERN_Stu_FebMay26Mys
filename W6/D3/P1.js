bf2bd66fc412fbf6dc409f2ef25a65c25cead344
varini666 <varinivarini925@gmail.com>
2026-03-25 11:12:19 +0530

    Intro to the NodeJS file system (fs) built-in module.js

--- W6/D3/P1.js
+++ W6/D3/P1.js
@@ -2,9 +2,9 @@
 
 const fs = require("fs");
 const fsPromises = require("fs/promises");
-
+// callback
 console.log("Type of fs.readFile:",typeof fs.readFile);
 console.log("Type of fs.writeFile:",typeof fs.writeFile);
-
+// fs/promises
 console.log("Type of fsPromises.readFile:",typeof fsPromises.readFile);
 console.log("Type of fsPromises.writeFile:",typeof fsPromises.writeFile);
