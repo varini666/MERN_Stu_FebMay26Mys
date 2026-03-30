a03d2de8363dca3c450e61152b716fc0edd57980
varini666 <varinivarini925@gmail.com>
2026-03-30 15:17:06 +0530

    Handling different HTTP methods in express.js

--- W7/D1/P3.js
+++ W7/D1/P3.js
@@ -5,6 +5,11 @@ const app = express();
 
 app.get("/users",function(req,res){
     // res.send() sends a response body and ends the request automatically
+        res.status(200).json([ 
+        {message:"success."},
+        {id:1,name:"varini"},
+        {id:2,name:"Aishu"},
+    ]);
     res.send("Returning all users");
 });
 
@@ -12,7 +17,7 @@ app.get("/users",function(req,res){
 app.post("/users",function(req,res){
     // res.status() sets the HTTP status code before sending the response body
     res.status(201).send("User created.");
-});
+}); 
 
 app.listen(4000,function(){
     console.log("Express server running at http://localhost:4000");
