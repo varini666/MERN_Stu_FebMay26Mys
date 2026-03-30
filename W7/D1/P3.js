f7e963530b15bbe53180148895001bd47688a023
varini666 <varinivarini925@gmail.com>
2026-03-30 11:15:01 +0530

    Handling different HTTP methods in express.js

---
+++ W7/D1/P3.js
@@ -0,0 +1,19 @@
+// Handling different HTTP methods in express
+const express = require("express");
+
+const app = express();
+
+app.get("/users",function(req,res){
+    // res.send() sends a response body and ends the request automatically
+    res.send("Returning all users");
+});
+
+// To create 
+app.post("/users",function(req,res){
+    // res.status() sets the HTTP status code before sending the response body
+    res.status(201).send("User created.");
+});
+
+app.listen(4000,function(){
+    console.log("Express server running at http://localhost:4000");
+});
