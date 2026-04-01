2ec0afec029b0e213e03a94eed68b36f4e62a042
varini666 <varinivarini925@gmail.com>
2026-04-01 13:00:02 +0530

    Generating token using login function & verifying token .js

---
+++ W7/D3/P6.js
@@ -0,0 +1,38 @@
+// Generating token using login function & verifying token 
+const jwt = require("jsonwebtoken");
+
+const secretKey = "monkey123";
+// const wrongsecretKey = "Don123";
+
+function loginUser(email,password){
+    if(email === "correct@email.com" && password === "mp123"){
+        const token = jwt.sign(
+            {
+                userId:101,
+                email:email,
+                role:"student"
+            },secretKey,{expiresIn:"1h"}
+        );
+    return{
+        success:true,
+        token:token
+    };    
+    }
+    return{
+        success:false,
+        message:"Invalid Credentials"
+    }; 
+}
+const loginResult = loginUser("correct@email.com","mp123");
+console.log("login result",loginResult);
+
+if(loginResult.success){
+    try{
+    const verifiedPayload = jwt.verify(loginResult.token,secretKey);    
+    // const verifiedPayload = jwt.verify(loginResult.token,wrongsecretKey);
+    console.log("verified payload",verifiedPayload);
+    }
+catch(error){
+    console.log("verification failed",error.message);
+}
+}
