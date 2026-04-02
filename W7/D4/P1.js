bf2ef2efc5afd8aa48b6b935f616e728cbe94fb7
varini666 <varinivarini925@gmail.com>
2026-04-02 13:15:02 +0530

    Basics of cookie.js

---
+++ W7/D4/P1.js
@@ -0,0 +1,14 @@
+// Basics of cookie
+const express = require("express");
+const app = express();
+
+app.get("/set-theme",function(req,res){
+    // res.cookie() tells the browser to store a cookie
+    res.cookie("theme","dark");
+
+    res.send("cookie named 'theme' with value 'dark' was sent to the browser");
+});
+
+app.listen(4000,function(){
+    console.log("JWT demo server running @ http://localhost:4000");
+});
