f34105b655d66cce50b5022491c56456c5394d99
varini666 <varinivarini925@gmail.com>
2026-04-16 15:03:14 +0530

    events

--- Team Project/CLI-based LMS/MERN_Week1_V1.0/events.js
+++ Team Project/CLI-based LMS/MERN_Week1_V1.0/events.js
@@ -1,4 +1,38 @@
-const EventEmitter = require('events');
+const EventEmitter = require("events");
+const chalk = require("chalk");
+
 const emitter = new EventEmitter();
+
+emitter.on("sessionStarted", (name) => {
+    console.log(chalk.cyan(`Welcome ${name}`));
+});
+
+emitter.on("courseViewed", () => {
+    console.log(chalk.blue("Viewing courses..."));
+});
+
+emitter.on("enrollmentStarted", () => {
+    console.log(chalk.yellow("Processing enrollment..."));
+});
+
+emitter.on("enrollmentConfirmed", (course) => {
+    console.log(chalk.green(`Enrolled in ${course}`));
+});
+
+emitter.on("lessonCompleted", (course) => {
+    console.log(chalk.green(`Lesson completed in ${course}`));
+});
+
+emitter.on("progressViewed", () => {
+    console.log(chalk.cyan("Showing progress..."));
+});
+
+emitter.on("courseWithdrawn", (course) => {
+    console.log(chalk.yellow(`Withdrawn from ${course}`));
+});
+
+emitter.on("operationFailed", (msg) => {
+    console.log(chalk.red(`Error: ${msg}`));
+});
 
 module.exports = emitter;
