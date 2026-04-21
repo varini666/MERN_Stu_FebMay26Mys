936f555fcb4cbe969ca9f651167b33c82c672fc5
varini666 <varinivarini925@gmail.com>
2026-04-21 10:02:29 +0530

    Basic server setup

---
+++ Projects/Guided Project/BookMyShowClone/MERN Week 3_4_GuidProject_V1_ExpressMongo/backend/server.js
@@ -0,0 +1,10 @@
+// Load environment variables
+require("dotenv").config();
+
+const app = require("./app");
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT,()=>{
+    console.log(`server running on port ${PORT}`);
+});
