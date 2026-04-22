c5bf4383d1e6b81e55d835c6ca47ade78f343990
varini666 <varinivarini925@gmail.com>
2026-04-22 19:52:35 +0530

    Fetching the bookings based on userId

--- Projects/Guided Project/BookMyShowClone/MERN_Week2_V1/routes/userRoutes.js
+++ Projects/Guided Project/BookMyShowClone/MERN_Week2_V1/routes/userRoutes.js
@@ -2,20 +2,11 @@
 const express = require("express");
 const {authMiddleware} = require("../middleware/authMiddleware");
 const {bookings} = require("../controllers/bookingController");
+const getBookingsByUserId = require("../controllers/userController");
 
 const router = express.Router();
 
 // get bookings for a specific user id
-router.get("/:userId/bookings",authMiddleware,(req,res)=>{
-    const userId = Number(req.params.userId);
-
-    const userBookings = bookings.filter((booking)=>
-    bookings.userId === userId);
-    res.status(200).json({
-        success:true,
-        count:userBookings.length,
-        data:userBookings
-    });
-});
+router.get("/:userId/bookings",authMiddleware,getBookingsByUserId);   //here we have written the function directly without creating the userController that's why we have created the file separately in the controller now we have 4 controller and 4 router, previously we had 3 controller and 4 routes 
 
 module.exports = router;
