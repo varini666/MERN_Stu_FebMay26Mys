5edc423e9f64db413b52e96ee8541fd0d7a08606
varini666 <varinivarini925@gmail.com>
2026-04-23 10:46:58 +0530

    Created CRUD operation functions with req & res and exported it

---
+++ Projects/Guided Project/BookMyShowClone/MERN Week 3_4_GuidProject_V1_ExpressMongo/backend/src/controllers/show.controller.js
@@ -0,0 +1,72 @@
+const showService = require("../services/show.service");
+
+// Create Show : Admin
+exports.createShow = async (req,res,next) =>{
+    try{
+        const show = await showService.createShow(req.body);
+        res.status(201).json({
+            success:true,
+            message:"Show created successfully",
+            data:show,
+        });
+    }
+    catch(error){
+        next(error);
+    }
+};
+
+// Get Shows
+exports.getShows = async (req,res,next) =>{
+    try{
+        const shows = await showService.getShows(req.query);
+        res.status(200).json({
+            success:true,
+            message:"Shows fetched successfully",
+            data:shows,
+        });
+    }
+    catch(error){
+        next(error);
+    }
+};
+// Get Single show
+exports.getShowById = async (req,res,next) =>{
+    try{
+        const show = await showService.getShowById(req.params.id);
+        res.status(200).json({
+            success:true,
+            message:"Show fetched successfully",
+            data:show,
+        });
+    }
+    catch(error){
+        next(error);
+    }
+};
+// Update show - admin
+exports.updateShow = async (req,res,next) =>{
+    try{
+        const show = await showService.updateShow(req.params.id,req.body);
+        res.status(200).json({
+            success:true,
+            message:"Show updated successfully",
+            data:show,
+        });
+    }
+    catch(error){
+        next(error);
+    }
+};
+// Delete show - admin (soft delete)
+exports.deleteShow = async (req,res,next) =>{
+    try{
+        const show = await showService.deleteShow(req.params.id);
+        res.status(200).json({
+            success:true,
+            message:"Show deleted successfully",
+        });
+    }
+    catch(error){
+        next(error);
+    }
+};
