d45480dffb2ad62a1029bf8d9ecb00b40eb747f8
varini666 <varinivarini925@gmail.com>
2026-04-23 12:16:31 +0530

    next is not recognized in new mongoose

--- Projects/Guided Project/BookMyShowClone/MERN Week 3_4_GuidProject_V1_ExpressMongo/backend/src/models/Show.js
+++ Projects/Guided Project/BookMyShowClone/MERN Week 3_4_GuidProject_V1_ExpressMongo/backend/src/models/Show.js
@@ -1,4 +1,5 @@
-const mongoose=require('mongoose');
+const mongoose = require("mongoose");
+// Sub schema
 const seatSchema = new mongoose.Schema({
     seatNumber:{
         type:String,
@@ -42,18 +43,25 @@ const showSchema = new mongoose.Schema({
         type:Boolean,
         default:true,
     },
-},{
-    timeStamps:true,
+},
+{
+    timestamps:true,
 });
 
-// compound index:
+//Compound index:
 showSchema.index({movieId:1,date:1});
 
-// Add validation
-showSchema.pre("save",function(next){
-    if(this.availableSeats>this.totalSeats){
-        return next(new Error("Available seats cannot exceed total seats"));
+//Add Validation
+// showSchema.pre("save",function(next){
+//     if (this.availableSeats>this.totalSeats) {
+//         return next(new Error("Available seats cannot exceed total seats"));
+//     }
+//     next();
+// });
+// New Add Validation code
+showSchema.pre("save", async function () {
+    if (this.availableSeats > this.totalSeats) {
+        throw new Error("Available seats cannot exceed total seats");
     }
-    next();
 });
-module.exports=mongoose.model("Show",showSchema);
+module.exports = mongoose.model("Show",showSchema);
