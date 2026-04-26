567848d2cc4a5a2c3094d0318937f00d37099786
varini666 <varinivarini925@gmail.com>
2026-04-26 19:46:58 +0530

    Added shows routes

--- Projects/Guided Project/BookMyShowClone/MERN Week 3_4_GuidProject_V1_ExpressMongo/backend/app.js
+++ Projects/Guided Project/BookMyShowClone/MERN Week 3_4_GuidProject_V1_ExpressMongo/backend/app.js
@@ -3,6 +3,8 @@ const authRoutes = require("./src/routes/auth.routes");
 const movieRoutes = require("./src/routes/movie.routes");
 const bookingRoutes = require("./src/routes/booking.routes");
 const showRoutes = require("./src/routes/show.routes");
+const errorMiddleware = require("./src/middleware/error.middleware");
+const morgan = require("morgan");
 
 const app = express();
 
@@ -11,6 +13,8 @@ app.use("/api/auth",authRoutes);
 app.use("/api/movies",movieRoutes);
 app.use("/api/bookings",bookingRoutes);
 app.use("/api/shows",showRoutes);
+app.use(errorMiddleware);
+app.use(morgan("dev"));
 
 //Base URL
 app.get("/",(req,res)=>{
