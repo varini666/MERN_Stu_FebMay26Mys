5b33307798b8770daf84ecdb29e31cd9e2745da0
varini666 <varinivarini925@gmail.com>
2026-04-28 15:40:35 +0530

    Prop destructuring

---
+++ W9/D2/prop-example-app/src/components/P2.jsx
@@ -0,0 +1,20 @@
+// Props destructuring
+import { React } from "react";
+
+function UserProfile({username,skill}){
+    return(
+        <div>
+            <p>User:{username}</p>
+            <p>Skill:{skill}</p>
+        </div>
+    )
+}
+// Parent component
+export function PropDestructuring(){
+    return(
+        <>
+        <h2>Prop Destructuring</h2>
+        <UserProfile username="varini" skill="React"/>
+        </>
+    );
+}
