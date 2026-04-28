411b6505e24b303127728b5aa1cef1c539d019f4
varini666 <varinivarini925@gmail.com>
2026-04-28 12:29:47 +0530

    Created simple functional components with props

---
+++ W9/D2/my-app/src/components/FunctionalComponentsBasics.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+function Welcome(props){
+    // Child component : reusable UI
+    return(
+        <p>Hello, {props.name}</p>
+    );
+}
+
+export function FunctionalComponentsBasics(){
+    return(
+        <div>
+            <h2>Functional Components Basics</h2>
+            {/* We are passing 'Varini' as prop Welcome function receives is as {name: "Varini"} */}
+            <Welcome name = "varini"/>
+            {/* We are passing 'Developer' as prop Welcome function receives is as {name: "Developer"} */}
+            <Welcome name = "Developer"/>
+            </div>
+    )
+}
