44bfaf4c93b4f72bbbf822776e58540546b3b769
varini666 <varinivarini925@gmail.com>
2026-04-29 12:12:20 +0530

    Composition

---
+++ W9/D2/prop-example-app/src/components/P6.jsx
@@ -0,0 +1,31 @@
+// Composition
+// import { React } from 'react';
+
+function DashboardLayout({children}){
+    return <div className="layout">{children}</div>
+} 
+function AccountSidebar({children}){
+    return <aside className="sidebar">{children}</aside>
+}
+function UserInfoPanel({children}){
+    return <aside className="user-panel">{children}</aside>
+}
+function ProfileAvatar({ user }){
+    return <img src={`/${user.name}.png`} alt={user.name} />
+}
+
+export function CompositionDemo(){
+    const user = {name: 'varini'};
+    return(
+        <div>
+            <h3>Composition Solution</h3>
+            <DashboardLayout>
+                <AccountSidebar>
+                    <UserInfoPanel>
+                        <ProfileAvatar user={user}/>
+                    </UserInfoPanel>
+                </AccountSidebar>
+            </DashboardLayout>
+        </div>
+    )
+}
