721be5db9aba505cf7bb4ada2e84853219b622b1
varini666 <varinivarini925@gmail.com>
2026-04-29 19:01:34 +0530

    Higher order components(HOC) example

---
+++ W9/D2/prop-example-app/src/components/P12.js
@@ -0,0 +1,26 @@
+// Higher order components(HOC) example
+
+const Greeting = ({name}) => {
+    return( 
+    <h1>Hello,{name}</h1>
+    )
+}
+
+const WithBorder = (OriginalComponent)=>{
+    return function EnhancedComponent(props){
+        return(
+            <div className="setBorder">
+                <OriginalComponent{...props}/>
+            </div>
+        );
+    }
+}
+const GreetingWithBorder = WithBorder(Greeting);
+
+export function HOCExample(){
+    return(
+        <>
+        <GreetingWithBorder name="varini"/>
+        </>
+    )
+}
