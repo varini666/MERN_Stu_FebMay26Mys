ba30933c739bb655ac4d5800b2a2cc98cc301c1b
varini666 <varinivarini925@gmail.com>
2026-05-06 12:35:17 +0530

    Derived state and conditional rendering

--- W10/D1/state-hooks-app/src/App.jsx
+++ W10/D1/state-hooks-app/src/App.jsx
@@ -1,12 +1,20 @@
 import './App.css'
 import { UseStateBasics } from './components/P1'
+import { UpdatingObjectsArraysState } from './components/P2'
+import { EventHandlingState } from './components/P3'
+import { DerivedStateCondRender } from './components/P4'
+import { UseEffectBasics } from './components/P5'
 
 function App() {
 
 
   return (
     <>
-    <UseStateBasics/>
+    {/* <UseStateBasics/> */}
+    {/* <UpdatingObjectsArraysState/> */}
+    {/* <EventHandlingState/> */}
+    {/* <DerivedStateCondRender/> */}
+    <UseEffectBasics/>
     </>
   )
 }
