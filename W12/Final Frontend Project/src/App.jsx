163d17c188c6ffcc8f8ef268ba97adf3beab77db
varini666 <varinivarini925@gmail.com>
2026-06-12 12:21:46 +0530

    Usage of Props

--- W12/Final Frontend Project/src/App.jsx
+++ W12/Final Frontend Project/src/App.jsx
@@ -1,15 +1,31 @@
-import './App.css'
-import { useState } from 'react';
-import MovieCard from './components/MovieCard';
-import HomePage from './pages/HomePage';
-
+import "./App.css";
+import MovieCard from "./components/MovieCard";
 function App() {
-    const [count, setCount] = useState(0)
-
     return (
-        //Component Composition
-        <HomePage/>
-    )
+        <>
+            <MovieCard
+                title="Inception"
+                genre="Sci-fi"
+                rating={4.8}
+                duration="2h 28m"
+                poster="https://picsum.photos/200/300?1"
+            />
+            <MovieCard
+                title="Avatar"
+                genre="Sci-fi"
+                rating={4.1}
+                duration="3h 28m"
+                poster="https://picsum.photos/200/300?2"
+            />
+            <MovieCard
+                title="FightClub"
+                genre="Sci-fi"
+                rating={4.9}
+                duration="2h 28m"
+                poster="https://picsum.photos/200/300?3"
+            />
+        </>
+    );
 }
 
-export default App
+export default App;
--- W12/Final Frontend Project/src/components/MovieCard.jsx
+++ W12/Final Frontend Project/src/components/MovieCard.jsx
@@ -1,19 +1,21 @@
-export default function MovieCard(){
-    const movie  = {
-        title:"Inception",
-        genre:"Sci-Fi",
-        rating:4.8,
-        duration:"2h 28m"
-    };
+export default function MovieCard(props){
+    // const movie = {
+    //     title: "Inception",
+    //     genre: "Sci-Fi",
+    //     rating: 4.8,
+    //     duration: "2h 28m"
+    // };
 
     return(
         <div className="movie-card">
-            <img src="https://picsum.photos/200/300" alt={movie.title} />
-            <h2>{movie.title}</h2>
-            <p>Genre: {movie.genre}</p>
-            <p>Rating:{movie.rating}</p>
-            <p>Duration:{movie.duration}</p>
+            {/* Destructuring  props*/}
+            <img src={props.poster} alt={props.title} />
+            <h3>{props.title}</h3>
+            <p>{[]}</p>
+            <p>Genre: {props.genre}</p>
+            <p>Rating: {props.rating}</p>
+            <p>Duration: {props.duration}</p>
             <button>Book Now</button>
         </div>
-    )
+    );
 }
